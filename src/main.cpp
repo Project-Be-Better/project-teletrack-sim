9d6829e98d0ca65009ea882ec51d43dbc6c5fa3d
sree <sreeraj_ec@hotmail.com>
2025-04-25 15:36:38 +0800

    feature-3: Implemented GNSS Simulation and Unit Tests

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,18 @@
 
 int main()
 {
-    std::cout << "GNSS Dummy : " << gnss() << std::endl;
+    gnss::GNSS gnss;
+
+    std::cout << "\nBefore Simulate: " << "\n"
+              << ">>> latitude: " << gnss.latitude() << "\n"
+              << ">>> longitude: " << gnss.longitude() << "\n";
+
+    gnss.simulate();
+
+    std::cout
+        << "\nAfter Simulate: " << "\n"
+        << ">>> latitude: " << gnss.latitude() << "\n"
+        << ">>> longitude: " << gnss.longitude() << "\n";
 
     return 0;
 }
